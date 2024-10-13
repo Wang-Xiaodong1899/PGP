c10da9073920a0e0a3c7a14e621eeee8e0ef89a8
Wang-Xiaodong1899 <1443472206@qq.com>
2024-10-14 01:51:45 +0800

    read pickle

---
+++ show.py
@@ -0,0 +1,8 @@
+import pickle
+
+file_path = "/workspace/wxd/PGP/data/ffffae497b0d4fb1947864f7210a237f_393a377273b3401a9f69c64e795f89ed.pickle"
+
+with open(file_path, "r") as f:
+    data = pickle.load(f)
+
+print(data)
