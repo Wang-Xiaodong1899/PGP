35abc4688964ec313a040adaf4982ff176f003fc
Wang-Xiaodong1899 <1443472206@qq.com>
2024-10-14 01:54:00 +0800

    fix

--- show.py
+++ show.py
@@ -2,7 +2,7 @@ import pickle
 
 file_path = "/workspace/wxd/PGP/data/ffffae497b0d4fb1947864f7210a237f_393a377273b3401a9f69c64e795f89ed.pickle"
 
-with open(file_path, "r", encoding="utf-8") as f:
+with open(file_path, "rb") as f:
     data = pickle.load(f)
 
 print(data)
