eb98400f21377f41ed1d4fd5fcc442399c5b9607
Wang-Xiaodong1899 <1443472206@qq.com>
2024-10-14 01:54:46 +0800

    minor

--- show.py
+++ show.py
@@ -5,4 +5,6 @@ file_path = "/workspace/wxd/PGP/data/ffffae497b0d4fb1947864f7210a237f_393a377273
 with open(file_path, "rb") as f:
     data = pickle.load(f)
 
+import pdb; pdb.set_trace()
+
 print(data)
